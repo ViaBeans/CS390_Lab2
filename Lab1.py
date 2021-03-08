11a90694a81954203c97d91621ca9748de378d41
Vishisht182 <jain322@purdue.edu>
2021-03-07 23:11:32 -0500

    Added regular ANN

--- Lab1.py
+++ Lab1.py
@@ -4,19 +4,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
+import math
 import random
 
 
 random.seed(1618)
 np.random.seed(1618)
-#tf.set_random_seed(1618)   # Uncomment for TF1.
+# tf.set_random_seed(1618)   # Uncomment for TF1.
 tf.random.set_seed(1618)
 
-#tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+# tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-ALGORITHM = "guesser"
-#ALGORITHM = "tf_net"
+#ALGORITHM = "guesser"
+ALGORITHM = "tf_net"
 #ALGORITHM = "tf_conv"
 
 DATASET = "mnist_d"
@@ -45,7 +46,71 @@ elif DATASET == "cifar_100_c":
     pass                                 # TODO: Add this case.
 
 
-#=========================<Classifier Functions>================================
+class ANN_net():
+    def __init__(self, inputSize, outputSize, neuronsPerLayer, learningRate=0.1):
+        self.inputSize = inputSize
+        self.outputSize = outputSize
+        self.neuronsPerLayer = neuronsPerLayer
+        self.lr = learningRate
+        self.W1 = np.random.randn(self.inputSize, self.neuronsPerLayer)
+        self.W2 = np.random.randn(self.neuronsPerLayer, self.outputSize)
+
+    # Activation function.
+    def __sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
+
+    # Activation prime function.
+    def __sigmoidDerivative(self, x):
+        fx = self.__sigmoid(x)
+        return fx * (1-fx)
+    # Batch generator for mini-batches. Not randomized.
+
+    def __batchGenerator(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i: i + n]
+
+    # Training with backpropagation.
+    def train(self, xVals, yVals, epochs=100000, minibatches=True, mbs=200):
+        x_batches = self.__batchGenerator(xVals, mbs)
+        y_batches = self.__batchGenerator(yVals, mbs)
+        for i in range(0, epochs):
+            if minibatches is True:
+                for x_b, y_b in zip(x_batches, y_batches):
+                    L1out, L2out = self.__forward(x_b)
+                    L2error = L2out - y_b
+                    L2delta = L2error * self.__sigmoidDerivative(L2out)
+                    L1error = np.dot(L2delta, self.W2.T)
+                    L1delta = L1error * self.__sigmoidDerivative(L1out)
+                    self.W1 -= x_b.T.dot(L1delta) * \
+                        self.lr * math.exp(-0.1 * i)
+                    self.W2 -= L1out.T.dot(L2delta) * \
+                        self.lr * math.exp(-0.1 * i)
+            else:
+                for img in range(0, xVals.shape[0]):
+                    L1out, L2out = self.__forward(xVals[img])
+                    L2error = L2out - yVals[img]
+                    L2delta = L2error * self.__sigmoidDerivative(L2out)
+                    L1error = np.dot(L2delta, self.W2.T)
+                    L1delta = L1error * self.__sigmoidDerivative(L1out)
+                    self.W1 -= xVals[img].T.dot(L1delta) * \
+                        self.lr * math.exp(-0.1 * i)
+                    self.W2 -= L1out.T.dot(L2delta) * \
+                        self.lr * math.exp(-0.1 * i)
+
+    # Forward pass.
+
+    def __forward(self, input):
+        layer1 = self.__sigmoid(np.dot(input, self.W1))
+        layer2 = self.__sigmoid(np.dot(layer1, self.W2))
+        return layer1, layer2
+
+    # Predict.
+    def predict(self, xVals):
+        _, layer2 = self.__forward(xVals)
+        return layer2
+
+# =========================<Classifier Functions>================================
+
 
 def guesserClassifier(xTest):
     ans = []
@@ -56,16 +121,18 @@ def guesserClassifier(xTest):
     return np.array(ans)
 
 
-def buildTFNeuralNet(x, y, eps = 6):
-    pass        #TODO: Implement a standard ANN here.
+def buildTFNeuralNet(x, y, eps=6):
+    ret = ANN_net(IS, NUM_CLASSES, 128)
+    ret.train(x, y, eps)
+    return ret
+
+
+def buildTFConvNet(x, y, eps=10, dropout=True, dropRate=0.2):
+    pass  # TODO: Implement a CNN here. dropout option is required.
     return None
 
+# =========================<Pipeline Functions>==================================
 
-def buildTFConvNet(x, y, eps = 10, dropout = True, dropRate = 0.2):
-    pass        #TODO: Implement a CNN here. dropout option is required.
-    return None
-
-#=========================<Pipeline Functions>==================================
 
 def getRawData():
     if DATASET == "mnist_d":
@@ -90,7 +157,6 @@ def getRawData():
     return ((xTrain, yTrain), (xTest, yTest))
 
 
-
 def preprocessData(raw):
     ((xTrain, yTrain), (xTest, yTest)) = raw
     if ALGORITHM != "tf_conv":
@@ -108,7 +174,6 @@ def preprocessData(raw):
     return ((xTrainP, yTrainP), (xTestP, yTestP))
 
 
-
 def trainModel(data):
     xTrain, yTrain = data
     if ALGORITHM == "guesser":
@@ -121,7 +186,6 @@ def trainModel(data):
         return buildTFConvNet(xTrain, yTrain)
     else:
         raise ValueError("Algorithm not recognized.")
-
 
 
 def runModel(data, model):
@@ -147,20 +211,19 @@ def runModel(data, model):
         raise ValueError("Algorithm not recognized.")
 
 
-
 def evalResults(data, preds):
     xTest, yTest = data
     acc = 0
     for i in range(preds.shape[0]):
-        if np.array_equal(preds[i], yTest[i]):   acc = acc + 1
+        if np.array_equal(preds[i], yTest[i]):
+            acc = acc + 1
     accuracy = acc / preds.shape[0]
     print("Classifier algorithm: %s" % ALGORITHM)
     print("Classifier accuracy: %f%%" % (accuracy * 100))
     print()
 
 
-
-#=========================<Main>================================================
+# =========================<Main>================================================
 
 def main():
     raw = getRawData()
@@ -168,7 +231,6 @@ def main():
     model = trainModel(data[0])
     preds = runModel(data[1][0], model)
     evalResults(data[1], preds)
-
 
 
 if __name__ == '__main__':
